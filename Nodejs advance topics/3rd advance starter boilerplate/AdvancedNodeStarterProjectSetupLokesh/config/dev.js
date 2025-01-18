9d0870007657235a441021be2bc07ead6103881f
lokesh903 <lavkeshchhatani@gmail.com>
2025-01-18 18:13:10 +0530

    caching using redis added

---
+++ Nodejs advance topics/3rd advance starter boilerplate/AdvancedNodeStarterProjectSetupLokesh/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:"15415794239-m5bfrtu00rn4fbt7vbikp1blv0ei4usg.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI:"mongodb://127.0.0.1:27017/blog_dev",
+  cookieKey: "123123123",
+};
